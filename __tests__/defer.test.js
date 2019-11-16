1c440fa0296b0dbd231de13700eed2ad176dff49
nerjs <karifan.stap@gmail.com>
2019-11-16 16:54:43 +0200

    add defr test

---
+++ __tests__/defer.test.js
@@ -0,0 +1,42 @@
+const DeferPromise = require('../defer')
+
+describe('Defer', () => {
+    test('is Promise', () => {
+        expect(new DeferPromise()).toBeInstanceOf(Promise)
+    })
+
+    test('outside methods', () => {
+        let resolveMethod, rejectMethod
+        const defer = new DeferPromise((resolve, reject) => {
+            resolveMethod = resolve
+            rejectMethod = reject
+        })
+
+        expect(resolveMethod).toStrictEqual(defer.resolve)
+        expect(rejectMethod).toStrictEqual(defer.reject)
+    })
+
+    test('using native Promise', () => {
+        let promise
+        promise = new DeferPromise(resolve => resolve('result'))
+
+        expect(promise).resolves.toEqual('result')
+
+        promise = new DeferPromise((_, reject) => reject(new Error('error')))
+
+        expect(promise).rejects.toThrow('error')
+    })
+
+    test('using defer Promise', () => {
+        let promise
+        promise = new DeferPromise()
+        promise.resolve('result')
+
+        expect(promise).resolves.toEqual('result')
+
+        promise = new DeferPromise()
+        promise.reject(new Error('error'))
+
+        expect(promise).rejects.toThrow('error')
+    })
+})
