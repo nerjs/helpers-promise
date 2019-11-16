fbc4cf05480f13aa23363ca3d5560ad4f4f80d2c
nerjs <karifan.stap@gmail.com>
2019-11-16 22:51:13 +0200

    asyncInterval tests

---
+++ __tests__/interval.test.js
@@ -0,0 +1,77 @@
+const asyncInterval = require('../interval')
+const sleep = require('../sleep')
+
+describe('Interval', () => {
+    beforeEach(() => {
+        jest.useFakeTimers()
+    })
+
+    afterEach(() => {
+        jest.useRealTimers()
+    })
+
+    test('use native interval', async () => {
+        const callback = jest.fn()
+        const stop = asyncInterval(callback, 50)
+
+        jest.runOnlyPendingTimers()
+        expect(setTimeout).toHaveBeenCalledTimes(1)
+        expect(callback).toHaveBeenCalled()
+        expect(callback).toHaveBeenCalledTimes(1)
+
+        jest.useRealTimers()
+        callback.mockRestore()
+        await sleep(220)
+
+        expect(callback).toHaveBeenCalledTimes(4)
+        stop()
+    })
+
+    test('use async interval', async () => {
+        jest.useRealTimers()
+        const callback = jest.fn()
+        const asyncCallback = async () => {
+            await sleep(50)
+            callback()
+        }
+
+        const stop = asyncInterval(asyncCallback, 50)
+
+        await sleep(250)
+        expect(callback).toHaveBeenCalledTimes(2)
+
+        stop()
+    })
+
+    test('error handler', async () => {
+        jest.useRealTimers()
+        const errorCallback = jest.fn()
+        const testError = new Error('Test')
+        const stop = asyncInterval(
+            async () => {
+                throw testError
+            },
+            errorCallback,
+            10,
+        )
+
+        await sleep(10)
+
+        expect(errorCallback).toHaveBeenCalled()
+        expect(errorCallback).toHaveBeenCalledTimes(1)
+
+        expect(errorCallback).toHaveBeenCalledWith(testError)
+
+        stop()
+    })
+
+    test('stop interval', () => {
+        const callback = jest.fn()
+
+        const stop = asyncInterval(callback, 10)
+        stop()
+        jest.runOnlyPendingTimers()
+
+        expect(callback).not.toHaveBeenCalled()
+    })
+})
--- interval.js
+++ interval.js
@@ -1,10 +1,26 @@
-const asyncInterval = (callback, time = 0) => {
+const asyncInterval = (callback, _errorCallback, _time = 0) => {
     if (!callback || typeof callback !== 'function')
         throw new TypeError('Callback should be a function')
+
+    let errorCallback = error => console.error(error)
+    let time = 0
+
+    if (typeof _errorCallback === 'function') {
+        errorCallback = _errorCallback
+        time = isNaN(Number(_time)) ? time : Number(_time)
+    } else if (typeof _errorCallback === 'number') {
+        time = _errorCallback
+    }
+
     let tid
 
     const interval = async () => {
-        await callback()
+        try {
+            await callback()
+        } catch (err) {
+            errorCallback(err)
+        }
+
         tid = setTimeout(interval, time)
     }
 
